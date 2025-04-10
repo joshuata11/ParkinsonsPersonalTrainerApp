cc6332e7f22f6b74ad9ca89de2827b87146ca180
CamLaws <camlaws1020@gmail.com>
2025-04-10 10:35:03 -0400

    Revert "created the calendar"
    This reverts commit 3535ce90d2004cce58f40ba4caefa58b1844ce74.

--- app/src/main/java/com/example/ppt/Home.kt
+++ app/src/main/java/com/example/ppt/Home.kt
@@ -11,7 +11,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatDelegate
-import com.example.ppt.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
